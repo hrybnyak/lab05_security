cdebba3aef3c41e6dfa93ee9f3a4debb2789f356
Yelyzaveta Hrybniak <y.s.hrybniak@globallogic.com>
2021-12-09 20:26:21 +0200

    scaffolde identity to project

---
+++ lab05_security/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using lab05_security.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(lab05_security.Areas.Identity.IdentityHostingStartup))]
+namespace lab05_security.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
